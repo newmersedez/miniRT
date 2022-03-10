6b0af823892bd4d3a1e64154449a0b1e26156c5b
Lynderly Orphan <lorphan@at-g3.msk.21-school.ru>
2022-03-10 21:02:07 +0300

    temp

--- includes/minirt.h
+++ includes/minirt.h
@@ -6,5 +6,31 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+typedef struct	s_pos
+{
+	double	x;
+	double	y;
+	double	z;
+}	t_pos;
+
+typedef struct	s_color
+{
+	int	r;
+	int	g;
+	int	b;
+}	t_color;
+
+typedef struct	s_ambient
+{
+	double	ratio;
+	t_color	color;
+}	t_ambient;
+
+typedef struct	s_camera
+{
+	t_pos	pos;
+	t_pos	normal_vec_pos;
+	double	fov;
+}	t_camera;
 
 #endif
