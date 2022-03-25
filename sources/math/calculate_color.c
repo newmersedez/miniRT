fe7a6cacf9b4278d798d763dc9bcbb470a173aa3
newmersedez <trishkk2001@gmail.com>
2022-03-25 23:06:32 +0300

    drawing plane, but bug with light

--- sources/math/calculate_color.c
+++ sources/math/calculate_color.c
@@ -3,35 +3,56 @@
 /*                                                        :::      ::::::::   */
 /*   calculate_color.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lorphan <lorphan@student.42.fr>            +#+  +:+       +#+        */
+/*   By: dmitry <dmitry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 20:39:51 by lorphan           #+#    #+#             */
-/*   Updated: 2022/03/24 22:07:57 by lorphan          ###   ########.fr       */
+/*   Updated: 2022/03/25 23:01:37 by dmitry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minirt.h"
+
+static t_vec	change_plane_normal(t_vec *normal_vec, t_vec *ray)
+{
+	t_vec	new_normal_vec;
+	double	dot_product;
+	double	cos_angle;
+
+	dot_product = vec_dot(normal_vec, ray);
+	cos_angle = dot_product / (vec_length(normal_vec) + vec_length(ray));
+	if (cos_angle >= 0 && cos_angle <= 1)
+		new_normal_vec = *normal_vec;
+	else
+		new_normal_vec = vec_multiply_by_num(normal_vec, -1);
+	return (new_normal_vec);
+}
 
 t_color	calculate_color(t_minirt *minirt, t_object *object, t_point *intersection_point)
 {
 	t_vec	normal_vec;
 	t_vec	light_vec;
 	t_color	color;
-	float	angle;
-	float	dot_product;
+	double	dot_product_light;
+	double	light_angle;
 
 	set_default_color(&color);
 	normal_vec = object->get_normal_vector(object->figure, intersection_point);
+	
 	light_vec = vec_subtract(intersection_point, &minirt->scene->light->pos);
 	light_vec = vec_normalize(&light_vec);
-	dot_product = vec_dot(&light_vec, &normal_vec);
-	angle = dot_product / (vec_length(&light_vec) * vec_length(&normal_vec));
-	if (angle >= 0 && angle <= 1)
+		
+	// if (object->type == PLANE)
+		// normal_vec = change_plane_normal(&normal_vec, &light_vec);
+	
+	dot_product_light = vec_dot(&light_vec, &normal_vec);
+	light_angle = dot_product_light / (vec_length(&light_vec) * vec_length(&normal_vec));
+	
+	if (light_angle >= 0 && light_angle <= 1)
 	{
 		color = object->color;
-		color.r = color.r * angle * minirt->scene->light->brightness_ratio;
-		color.g = color.g * angle * minirt->scene->light->brightness_ratio;
-		color.b = color.b * angle * minirt->scene->light->brightness_ratio;
+		color.r = color.r * light_angle * minirt->scene->light->brightness_ratio;
+		color.g = color.g * light_angle * minirt->scene->light->brightness_ratio;
+		color.b = color.b * light_angle * minirt->scene->light->brightness_ratio;
 	}
 	return (color);
 }
--- sources/math/ray_intersect_plane.c
+++ sources/math/ray_intersect_plane.c
@@ -3,14 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   ray_intersect_plane.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lorphan <lorphan@student.42.fr>            +#+  +:+       +#+        */
+/*   By: dmitry <dmitry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 17:00:38 by lorphan           #+#    #+#             */
-/*   Updated: 2022/03/24 20:37:51 by lorphan          ###   ########.fr       */
+/*   Updated: 2022/03/25 22:40:52 by dmitry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minirt.h"
+
+static t_vec	change_plane_normal(t_vec *normal_vec, t_vec *ray)
+{
+	t_vec	new_normal_vec;
+	double	dot_product;
+	double	cos_angle;
+
+	dot_product = vec_dot(normal_vec, ray);
+	cos_angle = dot_product / (vec_length(normal_vec) + vec_length(ray));
+	if (cos_angle >= 0 && cos_angle <= 1)
+		new_normal_vec = *normal_vec;
+	else
+		new_normal_vec = vec_multiply_by_num(normal_vec, -1);
+	return (new_normal_vec);
+}
 
 t_point	ray_intersect_plane(const void *data, const t_point *start_point,
 			const t_vec *ray)
@@ -18,6 +33,7 @@ t_point	ray_intersect_plane(const void *data, const t_point *start_point,
 	t_point	point;
 	t_plane	*plane;
 	t_vec	ray_normal;
+	t_vec	plane_normal;
 	t_vec	vec;
 	double	denom;
 	double	t;
@@ -25,13 +41,29 @@ t_point	ray_intersect_plane(const void *data, const t_point *start_point,
 	plane = (t_plane *)data;
 	set_default_point(&point);
 	ray_normal = vec_normalize(ray);
-	denom = vec_dot(&plane->normal, &ray_normal);
-	if (denom > 1e-6)
+	plane_normal = change_plane_normal(&plane->normal, ray);
+	denom = vec_dot(&plane_normal, &ray_normal);
+	if (denom > 0.0001f)
 	{
 		vec = vec_subtract(start_point, &plane->pos);
-		t = vec_dot(&vec, &plane->normal) / denom;
-		point = vec_multiply_by_num(ray, t);
-		point = vec_add(start_point, &point);
+		t = vec_dot(&vec, &plane_normal) / denom;
+		if (t >= 0)
+		{
+			point = vec_multiply_by_num(ray, t);
+			point = vec_add(start_point, &point);
+		}
 	}
+	// if (point.x != INFINITY && point.y != INFINITY && point.z != INFINITY)
+		// display_vector(&point);
 	return (point);
 }
+
+// t_point	ray_intersect_plane(const void *data, const t_point *start_point,
+			// const t_vec *ray)
+// {
+	// t_point	point;
+	// t_plane	*plane;
+// 
+	// plane = (t_plane *)plane;
+	// 
+// }
