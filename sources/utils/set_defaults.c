354775dc964918c484fa1bcb68c3a6cf62b43570
newmersedez <trishkk2001@gmail.com>
2022-03-31 15:26:10 +0300

    temp push for school

--- sources/utils/set_defaults.c
+++ sources/utils/set_defaults.c
@@ -6,7 +6,7 @@
 /*   By: dmitry <dmitry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 16:58:24 by lorphan           #+#    #+#             */
-/*   Updated: 2022/03/31 02:12:57 by dmitry           ###   ########.fr       */
+/*   Updated: 2022/03/31 15:16:19 by dmitry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ void	set_default_color(t_color *color)
 
 void	set_sky_color(t_minirt *minirt, t_color *color)
 {
-	color->r = 135 * minirt->scene->light->brightness_ratio;
-	color->g = 206 * minirt->scene->light->brightness_ratio;
-	color->b = 235 * minirt->scene->light->brightness_ratio;
+	color->r = 0 * minirt->scene->light->brightness_ratio;
+	color->g = 150 * minirt->scene->light->brightness_ratio;
+	color->b = 255 * minirt->scene->light->brightness_ratio;
 }
 
 void	set_default_point(t_point *point)
